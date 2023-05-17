02cab7eb7a23576a7e1db0891c850cf85c5da3b0
mihaneyano <131777676+mihaneyano@users.noreply.github.com>
2023-05-17 09:44:52 +0000

    [add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+devise_for :users
 root to: "homes#top"
+
+get "/home/about" => "homes#about", as: "about"
+
+resources :users, only: [:top, :index, :show, :edit, :update]
+
+resources :books, only: [:index, :show]
+
 
 end
